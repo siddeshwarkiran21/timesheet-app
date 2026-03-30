f4678d34610f1fb6655769ca9ebca5413235ed43
kiran043e <kiransiddeshwar@gmail.com>
2026-03-30 11:27:11 +0530

    login route changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Login from "./login/page";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl">Timesheet App</h1>
+    <div>
+      <Login />
     </div>
   );
 }
